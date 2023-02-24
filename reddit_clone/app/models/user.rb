0690b661c5fa3f898f44eb02a45cfb03455fe718
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-02-24 11:50:50 -0500

    Users model finished

--- reddit_clone/app/models/user.rb
+++ reddit_clone/app/models/user.rb
@@ -29,6 +29,8 @@ class User < ApplicationRecord
 
     before_validation :ensure_session_token
 
+    attr_reader :password
+
     
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
@@ -50,6 +52,24 @@ class User < ApplicationRecord
         end
         token
     end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
+    end
+
+
+
 
     
 
