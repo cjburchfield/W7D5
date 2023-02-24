2052b554a9c538fcd1333f8d3ed5c78035798a38
Morgan Waddington <waddington.morgan@gmail.com>
2023-02-24 11:25:08 -0500

    first half of auth

--- reddit_clone/app/models/user.rb
+++ reddit_clone/app/models/user.rb
@@ -9,5 +9,48 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
+# F - self.find_by_credientials
+# I - is_password?(password)
+# G - generate_session_token
+# V - validations
+# A - attr_reader
+# P - password=(password)
+# E - ensure_session_toke
+# R - reset_session_token
+
+
+
+
 class User < ApplicationRecord
+
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    before_validation :ensure_session_token
+
+    
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && User.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def generate_session_token
+        token = SecureRandon::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandon::urlsafe_base64
+        end
+        token
+    end
+
+    
+
 end
